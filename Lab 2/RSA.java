da7266f006763c6564caf3d6e9f253e57253092c
krkan <krkandrkan@gmail.com>
2015-09-06 13:17:58 +0200

    Some minor changes in lab 2 code.

--- Lab 2/RSA.java
+++ Lab 2/RSA.java
@@ -5,42 +5,40 @@ import java.security.*;
 
 public class RSA {
 
-	private String randomPrime() {
+	private BigInteger randomPrime() {
 		SecureRandom random = new SecureRandom();
 		BigInteger prime = BigInteger.probablePrime(512, random);
-		return prime + "";
+		return prime;
 	}
 
-	private BigInteger p_prime = new BigInteger(randomPrime());
-	private BigInteger q_prime = new BigInteger(randomPrime());
-	private BigInteger n = p_prime.multiply(q_prime);
+	private BigInteger p_prime = randomPrime();
+	private BigInteger q_prime = randomPrime();
 	private BigInteger phi_n = p_prime.subtract(BigInteger.valueOf(1))
 			.multiply(q_prime.subtract(BigInteger.valueOf(1)));
 	
-	// e is the public key.
-	//private BigInteger e = BigInteger.valueOf(65537);
-	private BigInteger e = new BigInteger(randomPrime());
+	// e & n together is the public key.
+	private BigInteger e = randomPrime();
+	private BigInteger n = p_prime.multiply(q_prime);
 	
-	
-	// d is the generated private key.
+	// d is the generated private key we do not share.
 	private BigInteger d = e.modInverse(phi_n);
 
-	// The message that will be encrypted and decrypted.
+
+	// Send e & n to the client so they can encrypt their message "msg". C is the
+	// encrypted message.
+	
 	private String msg = "Ibrahimovic";
 	private BigInteger m = new BigInteger(msg.getBytes());
-
-	// Send e & n to the client so they can encrypt their message m. C is the
-	// encrypted message.
 	private BigInteger c = m.modPow(e, n);
 
 	private void printResults() {
 		System.out.println("GCD of e and phi_n = " + phi_n.gcd(e));
-		System.out.println("Public key to be sent = " + e);
-		System.out.println("N to be sent = " + n);
+		System.out.println("E to be sent = " + e);
+		System.out.println("N to be sent = " + n + "\n");
 
 		System.out.println("Number to be encrypted = " + m);
 		System.out.println("Message to be encrypted = " + msg);
-		// Decrypt
+		// Decrypt using d & n
 		System.out.println("Number that was decrypted = " + c.modPow(d, n));
 		System.out.println("Decrypted message = " + new String(c.modPow(d, n).toByteArray()));
 	}
