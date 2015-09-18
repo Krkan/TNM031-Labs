8a72907ca884740e103e5892fd3d61c8407ba43d
krkan <krkandrkan@gmail.com>
2015-09-18 21:50:05 +0200

    Labb3, delete now works.

--- Lab 3 SSL_Client/SSL_Client.java
+++ Lab 3 SSL_Client/SSL_Client.java
@@ -8,59 +8,114 @@ public class SSL_Client {
 	private String _PASSWORD = "changeit";
 	private int _PORT = 43000;
 	private int FILE_SIZE = 6022386;
-
+	
+	private SSLSocketFactory sslsocketfactory;
+	private SSLSocket sslsocket; 
+	private InputStream inputstream;
+	private InputStreamReader inputstreamreader;
+	private BufferedReader bufferedreader;
+	private OutputStream outputstream;
+	private OutputStreamWriter outputstreamwriter;
+	private BufferedWriter bufferedwriter;
+	private String choice;
+	
 	private void connect() {
 		try {
 
 			System.setProperty("javax.net.ssl.trustStore", _TRUSTSTORE);
 			System.setProperty("javax.net.ssl.trustStorePassword", _PASSWORD);
 
-			SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-			SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket("localhost", _PORT);
+			sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+			sslsocket = (SSLSocket) sslsocketfactory.createSocket("localhost", _PORT);
 
-			InputStream inputstream = System.in;
-			InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
-			BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
+			inputstream = System.in;
+			inputstreamreader = new InputStreamReader(inputstream);
+			bufferedreader = new BufferedReader(inputstreamreader);
 
-			OutputStream outputstream = sslsocket.getOutputStream();
-			OutputStreamWriter outputstreamwriter = new OutputStreamWriter(outputstream);
-			BufferedWriter bufferedwriter = new BufferedWriter(outputstreamwriter);
+			outputstream = sslsocket.getOutputStream();
+			outputstreamwriter = new OutputStreamWriter(outputstream);
+			bufferedwriter = new BufferedWriter(outputstreamwriter);
 
-			System.out.print("Please enter a file name with extension: ");
-
-			String string = null;
-			string = bufferedreader.readLine();
-
-			bufferedwriter.write(string + '\n');
+			System.out.println("1. Download file\n2. Upload file\n3. Delete file \n");
+			
+			choice = bufferedreader.readLine();
+			
+			bufferedwriter.write(choice + '\n');
 			bufferedwriter.flush();
+			
+			if (choice.equals("1")){
+				
 
-			byte[] mybytearray = new byte[FILE_SIZE];
-			InputStream is = sslsocket.getInputStream();
-			FileOutputStream fos = new FileOutputStream(string);
-			BufferedOutputStream bos = new BufferedOutputStream(fos);
-			int bytesRead = is.read(mybytearray, 0, mybytearray.length);
-			int current = bytesRead;
-
-			do {
-				bytesRead = is.read(mybytearray, current, (mybytearray.length - current));
-				if (bytesRead >= 0)
-					current += bytesRead;
-			} while (bytesRead > -1);
-
-			bos.write(mybytearray, 0, current);
-			bos.flush();
-			System.out.println("File " + string + " downloaded (" + current + " bytes read)");
-
-			inputstream.close();
-			outputstream.close();
-			fos.close();
-			bos.close();
+			this.download();
+			
+			}else if(choice.equals("3")){
+				
+				this.delete();
+			}
 			sslsocket.close();
+
+			
 		} catch (Exception exception) {
-			 exception.printStackTrace();
+			 //exception.printStackTrace();
 			System.out.println("Lost connection to server.");
 
 		}
+	}
+	
+	
+	private void download(){
+		try{
+		System.out.print("Please enter a file name with extension: ");
+
+		String string = null;
+		string = bufferedreader.readLine();
+
+		bufferedwriter.write(string + '\n');
+		bufferedwriter.flush();
+
+		byte[] mybytearray = new byte[FILE_SIZE];
+		InputStream is = sslsocket.getInputStream();
+		FileOutputStream fos = new FileOutputStream(string);
+		BufferedOutputStream bos = new BufferedOutputStream(fos);
+		int bytesRead = is.read(mybytearray, 0, mybytearray.length);
+		int current = bytesRead;
+
+		do {
+			bytesRead = is.read(mybytearray, current, (mybytearray.length - current));
+			if (bytesRead >= 0)
+				current += bytesRead;
+		} while (bytesRead > -1);
+
+		bos.write(mybytearray, 0, current);
+		bos.flush();
+		System.out.println("File " + string + " downloaded (" + current + " bytes read)");
+
+		inputstream.close();
+		outputstream.close();
+		fos.close();
+		bos.close();
+		}catch (Exception e){
+			System.out.println("Error in dowloand function.");
+		}
+		
+		
+	}
+	
+	private void delete(){
+		
+		try{
+		System.out.print("Please enter a file name with extension to delete: ");
+
+		String string = null;
+		string = bufferedreader.readLine();
+
+		bufferedwriter.write(string + '\n');
+		bufferedwriter.flush();
+		System.out.println("File " + string + " deleted!");
+		}catch(Exception e){
+			System.out.println("Error in delete function.");
+		}
+		
 	}
 
 	public static void main(String[] arstring) {
--- Lab 3 SSL_Server/SSL_Server.java
+++ Lab 3 SSL_Server/SSL_Server.java
@@ -16,39 +16,39 @@ public class SSL_Server {
 	private String _PASSWORD = "changeit";
 	private int _PORT = 43000;
 
+	private SSLServerSocketFactory sslserversocketfactory;
+	private SSLServerSocket sslserversocket;
+	private SSLSocket sslsocket;
+	private InputStream inputstream;
+	private InputStreamReader inputstreamreader;
+	private BufferedReader bufferedreader;
+	private String choice;
+	
 	private void startServer() {
 
 		System.setProperty("javax.net.ssl.keyStore", _KEYSTORE);
 		System.setProperty("javax.net.ssl.keyStorePassword", _PASSWORD);
 		try {
-			SSLServerSocketFactory sslserversocketfactory = (SSLServerSocketFactory) SSLServerSocketFactory
+			sslserversocketfactory = (SSLServerSocketFactory) SSLServerSocketFactory
 					.getDefault();
-			SSLServerSocket sslserversocket = (SSLServerSocket) sslserversocketfactory.createServerSocket(_PORT);
+			sslserversocket = (SSLServerSocket) sslserversocketfactory.createServerSocket(_PORT);
 			System.out.println("Awaiting conection...");
 
-			SSLSocket sslsocket = (SSLSocket) sslserversocket.accept();
+			sslsocket = (SSLSocket) sslserversocket.accept();
 
-			InputStream inputstream = sslsocket.getInputStream();
-			InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
-			BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
+			inputstream = sslsocket.getInputStream();
+			inputstreamreader = new InputStreamReader(inputstream);
+			bufferedreader = new BufferedReader(inputstreamreader);
 
-			String string = null;
-			string = bufferedreader.readLine();
-
-			File myFile = new File(string);
-			byte[] mybytearray = new byte[(int) myFile.length()];
-			FileInputStream fis = new FileInputStream(myFile);
-			BufferedInputStream bis = new BufferedInputStream(fis);
-			bis.read(mybytearray, 0, mybytearray.length);
-			OutputStream os = sslsocket.getOutputStream();
-			System.out.println("Sending " + string + "(" + mybytearray.length + " bytes)");
-			os.write(mybytearray, 0, mybytearray.length);
-			os.flush();
-			System.out.println("Done.");
-
-			bis.close();
-			inputstream.close();
-			os.close();
+			choice = bufferedreader.readLine();
+			
+			if (choice.equals("1")){
+				this.uploadFile();
+			}
+			else if(choice.equals("3")){
+				this.deleteFile();
+		
+		}
 			sslsocket.close();
 
 		} catch (Exception exception) {
@@ -56,6 +56,48 @@ public class SSL_Server {
 			System.out.println("Client closed connection.");
 		}
 
+	}
+	
+	
+	private void uploadFile(){
+		
+		try{
+		String string = null;
+		string = bufferedreader.readLine();
+
+		File myFile = new File(string);
+		byte[] mybytearray = new byte[(int) myFile.length()];
+		FileInputStream fis = new FileInputStream(myFile);
+		BufferedInputStream bis = new BufferedInputStream(fis);
+		bis.read(mybytearray, 0, mybytearray.length);
+		OutputStream os = sslsocket.getOutputStream();
+		System.out.println("Sending " + string + "(" + mybytearray.length + " bytes)");
+		os.write(mybytearray, 0, mybytearray.length);
+		os.flush();
+		System.out.println("Done.");
+
+		bis.close();
+		inputstream.close();
+		os.close();
+		}catch(Exception e){
+			System.out.println("Error in upload function.");
+		}
+		
+	}
+	
+	private void deleteFile(){
+		
+		try{
+			String string = null;
+			string = bufferedreader.readLine();
+			File myFile = new File(string);
+			myFile.delete();
+			System.out.println("Deleted file " + string);
+			
+		}catch(Exception e){
+			System.out.println("Error in delete function.");
+		}
+		
 	}
 
 	public static void main(String[] arstring) {
